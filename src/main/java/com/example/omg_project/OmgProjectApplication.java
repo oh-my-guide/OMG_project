2f37f444c69eb138da06810a13fc02472138a98d
h_jinnny <nd3807@naver.com>
2024-07-30 15:56:51 +0900

    first commit

---
+++ src/main/java/com/example/omg_project/OmgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.omg_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmgProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmgProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/omg_project/OmgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omg_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmgProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
