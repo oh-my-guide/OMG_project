df774a298c246ed98f00fce5729067e7fdd29d36
h_jinnny <nd3807@naver.com>
2024-08-05 14:05:11 +0900

    remove: chat에 필요한 user 테스트 코드 제거

--- src/main/java/com/example/omg_project/domain/chat/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.omg_project.domain.chat.repository;
-
-
-import com.example.omg_project.domain.role.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/com/example/omg_project/domain/chat/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.omg_project.domain.chat.service;
-
-import com.example.omg_project.domain.user.entity.User;
-
-public interface UserService {
-    User findByUsername(String username);
-
-    User registerNewUser(User user);
-}
