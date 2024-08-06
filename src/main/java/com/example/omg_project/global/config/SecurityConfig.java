2f1d9e0b9664b1f0b8f58c7b985e0c06045ff84c
kyongseo <pokj930@naver.com>
2024-08-06 14:24:17 +0900

    refactor: 회원가입 로직 예외 처리 추가

--- src/main/java/com/example/omg_project/global/config/SecurityConfig.java
+++ src/main/java/com/example/omg_project/global/config/SecurityConfig.java
@@ -1,46 +1,54 @@
 package com.example.omg_project.global.config;
 
-import com.example.omg_project.global.security.CustomUserDetailsService;
+import com.example.omg_project.global.jwt.exception.CustomAuthenticationEntryPoint;
+import com.example.omg_project.global.jwt.filter.JWTFilter;
+import com.example.omg_project.global.jwt.service.JwtBlacklistService;
+import com.example.omg_project.global.jwt.service.RefreshTokenService;
+import com.example.omg_project.global.jwt.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final JwtBlacklistService jwtBlacklistService;
+    private final RefreshTokenService refreshTokenService;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers("/signup", "/signin", "/", "/css/**", "/js/**", "/files/**").permitAll()
+                        .requestMatchers("/api/login","/api/signup").permitAll()
                         .requestMatchers("/api/mail/**","/api/verify-code", "/api/check-email","/api/check-usernick").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/signin")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/mypage")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                )
+                .addFilterBefore(new JWTFilter(jwtUtil, jwtBlacklistService, refreshTokenService), UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form -> form.disable())
                 .sessionManagement(sessionManagement -> sessionManagement
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(true)
-                )
-                .userDetailsService(customUserDetailsService)
-                .csrf(csrf -> csrf.disable()
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
                 );
         return http.build();
     }
@@ -48,6 +56,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("http://localhost:3000"); // 프론트의 주소
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
