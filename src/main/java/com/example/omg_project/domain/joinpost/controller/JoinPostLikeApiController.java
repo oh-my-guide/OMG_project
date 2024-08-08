9c5d0b0234d3c8e38db968848b2ddd3f64dff67f
seolb <seolb2107@hanmail.net>
2024-08-08 18:27:55 +0900

    feat: 좋아요 기능 구현
    - 좋아요 기능 구현
    
    Related to: #5

---
+++ src/main/java/com/example/omg_project/domain/joinpost/controller/JoinPostLikeApiController.java
@@ -0,0 +1,34 @@
+package com.example.omg_project.domain.joinpost.controller;
+
+import com.example.omg_project.domain.joinpost.service.JoinPostLikeService;
+import com.example.omg_project.domain.user.entity.User;
+import com.example.omg_project.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/joinPosts")
+@RequiredArgsConstructor
+public class JoinPostLikeApiController {
+
+    private final JoinPostLikeService joinPostLikeService;
+    private final UserService userService;
+
+    @GetMapping("/{joinPostId}/likes")
+    public ResponseEntity<Map<String, Object>> getLikeInfo (@PathVariable Long joinPostId, Authentication authentication) {
+        User user = userService.findByUsername(authentication.getName()).orElseThrow();
+        Map<String, Object> likeInfo = joinPostLikeService.getLikeInfo(user.getId(), joinPostId);
+        return ResponseEntity.ok(likeInfo);
+    }
+
+    @PostMapping("/{joinPostId}/likes")
+    public ResponseEntity<Void> toggleLike(@PathVariable Long joinPostId, Authentication authentication) {
+        User user = userService.findByUsername(authentication.getName()).orElseThrow();
+        joinPostLikeService.toggleLike(user.getId(), joinPostId);
+        return ResponseEntity.ok().build();
+    }
+}
