733affd9af60b1f03ffc2b904574a01f302fd45f
kyongseo <pokj930@naver.com>
2024-08-08 14:14:52 +0900

    feat: OAuth2 로그인 구현

---
+++ src/main/java/com/example/omg_project/global/oauth2/handle/CustomSuccessHandler.java
@@ -0,0 +1,82 @@
+package com.example.omg_project.global.oauth2.handle;
+
+import com.example.omg_project.global.jwt.entity.RefreshToken;
+import com.example.omg_project.global.jwt.service.RefreshTokenService;
+import com.example.omg_project.global.jwt.util.JwtTokenizer;
+import com.example.omg_project.global.oauth2.dto.CustomOAuth2User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static com.example.omg_project.global.jwt.util.JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenizer jwtTokenizer;
+    private final RefreshTokenService refreshTokenService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        try {
+            CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
+
+            Long userId = customUserDetails.getUserIdAsLong();
+            String username = customUserDetails.getUsername();
+            String name = customUserDetails.getName();
+            List<String> roles = customUserDetails.getRoles();
+
+            log.info("Oauth2 로그인 성곻했습니다. ");
+            log.info("jwt 토큰 생성 :: userId: {}, username: {}, name: {}, roles: {}", userId, username, name, roles);
+
+            String accessToken = jwtTokenizer.createAccessToken(userId, username, name, roles);
+            String refreshToken = jwtTokenizer.createRefreshToken(userId, username, name, roles);
+
+            log.info("Access Token :: {}", accessToken);
+            log.info("Refresh Token :: {}", refreshToken);
+
+            // 쿠키에 토큰 저장
+            Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+            accessTokenCookie.setHttpOnly(true);
+            accessTokenCookie.setPath("/");
+            accessTokenCookie.setMaxAge(Math.toIntExact(jwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
+
+            Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+            refreshTokenCookie.setHttpOnly(true);
+            refreshTokenCookie.setPath("/");
+            refreshTokenCookie.setMaxAge(Math.toIntExact(jwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT / 1000));
+
+            response.addCookie(accessTokenCookie);
+            response.addCookie(refreshTokenCookie);
+
+            // 리프레시 토큰 DB 저장
+            Date date = new Date(System.currentTimeMillis() + jwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT);
+            RefreshToken refreshTokenEntity = new RefreshToken();
+            refreshTokenEntity.setValue(refreshToken);
+            refreshTokenEntity.setUserId(userId);
+            refreshTokenEntity.setExpiration(date.toString());
+
+            refreshTokenService.addRefreshToken(refreshTokenEntity);
+
+            response.sendRedirect("/oauthPage");
+
+        } catch (Exception e) {
+            log.error("Oauth2 로그인에 실패했습니다.", e);
+            if (!response.isCommitted()) {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during authentication");
+            }
+        }
+    }
+}
