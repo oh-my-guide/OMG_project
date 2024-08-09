ea28f2ec6df75129fa299a20e20fab17956beb70
seolb <seolb2107@hanmail.net>
2024-08-09 15:08:22 +0900

    refactor: Authentication 객체 사용
    - Principal -> Authentication 로 수정
    
    Related to: #25

--- src/main/java/com/example/omg_project/domain/joinpost/controller/JoinPostController.java
+++ src/main/java/com/example/omg_project/domain/joinpost/controller/JoinPostController.java
@@ -5,13 +5,13 @@ import com.example.omg_project.domain.joinpost.service.JoinPostService;
 import com.example.omg_project.domain.user.entity.User;
 import com.example.omg_project.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -32,13 +32,13 @@ public class JoinPostController {
     }
 
     @GetMapping("/{postId}")
-    public String viewPost(@PathVariable Long postId, Principal principal, Model model) {
+    public String viewPost(@PathVariable Long postId, Authentication authentication, Model model) {
         // 게시글 가져오기
         JoinPostDto.Response post = joinPostService.findJoinPostById(postId);
         model.addAttribute("post", post);
 
         // 로그인한 사용자 정보 가져오기
-        Optional<User> byUsername = userService.findByUsername(principal.getName());
+        Optional<User> byUsername = userService.findByUsername(authentication.getName());
         byUsername.ifPresent(user -> model.addAttribute("user", user));
 
         return "join/viewpost";
