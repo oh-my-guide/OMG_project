bae9242b1ae16d9f02bfeed348828710f0ebbe4a
Hyeju Kim <kimoju03@naver.com>
2024-08-12 13:02:41 +0900

    comment: 사용하지 않는 클래스 임시 주석 처리
    Related to: #22

--- src/main/java/com/example/omg_project/domain/trip/dto/TripLocationDto.java
+++ src/main/java/com/example/omg_project/domain/trip/dto/TripLocationDto.java
@@ -1,15 +1,15 @@
-package com.example.omg_project.domain.trip.dto;
-
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TripLocationDto {
-
-    private String name;
-    private Double longitude;
-    private Double latitude;
-
-}
+//package com.example.omg_project.domain.trip.dto;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class TripLocationDto {
+//
+//    private String name;
+//    private Double longitude;
+//    private Double latitude;
+//
+//}
