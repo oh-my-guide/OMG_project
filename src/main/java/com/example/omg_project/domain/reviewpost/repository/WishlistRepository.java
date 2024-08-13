e868f5791936c08d3667cd5740497b210cad1396
seolb <seolb2107@hanmail.net>
2024-08-13 12:00:25 +0900

    feat: 찜하기 기능 구현
    - 후기게시판 찜하기 기능 추가
    
    Resolves: #58

--- src/main/java/com/example/omg_project/domain/reviewpost/repository/WishlistRepository.java
+++ src/main/java/com/example/omg_project/domain/reviewpost/repository/WishlistRepository.java
@@ -3,5 +3,10 @@ package com.example.omg_project.domain.reviewpost.repository;
 import com.example.omg_project.domain.reviewpost.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+    List<Wishlist> findAllByUserId(Long userId);
+    Wishlist findByUserIdAndReviewPostId(Long userId, Long reviewPostId);
+    boolean existsByUserIdAndReviewPostId(Long userId, Long reviewPostId);
 }
