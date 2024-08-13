b1037f8d86c4d599f0b90944c9db4e70ee0d5017
jini <dbwls20202554@gmail.com>
2024-08-13 12:08:44 +0900

    refactor: 여행 일정 수정, 사용자 일정 리스트 컨트롤러에 user 객체 포함시킴

--- src/main/java/com/example/omg_project/domain/trip/controller/TripController.java
+++ src/main/java/com/example/omg_project/domain/trip/controller/TripController.java
@@ -72,16 +72,25 @@ public class TripController {
 
     //사용자의 일정 리스트 표시
     @GetMapping("/user/{userId}")
-    public String viewTripsByUserIdPage(@PathVariable Long userId, Model model) {
+    public String viewTripsByUserIdPage(@PathVariable Long userId, Model model, HttpServletRequest request) {
         List<ReadTripDTO> trips = tripService.getTripsByUserId(userId);
+        String accessToken = jwtTokenizer.getAccessTokenFromCookies(request);
+        String username = jwtTokenizer.getUsernameFromToken(accessToken);
+        User user = userService.findByUsername(username).orElseThrow();
+        model.addAttribute("user", user);
         model.addAttribute("trips", trips);
+
         return "trip/usertrip";
     }
 
     //여행 일정 수정 페이지
     @GetMapping("/update/{id}")
-    public String showUpdateTripPage(@PathVariable Long id, Model model) {
+    public String showUpdateTripPage(@PathVariable Long id, Model model, HttpServletRequest request) {
         ReadTripDTO trip = tripService.getTripById(id);
+        String accessToken = jwtTokenizer.getAccessTokenFromCookies(request);
+        String username = jwtTokenizer.getUsernameFromToken(accessToken);
+        User user = userService.findByUsername(username).orElseThrow();
+        model.addAttribute("user", user);
         model.addAttribute("trip", trip);
         return "trip/updateTrip";
     }
