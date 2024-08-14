4c68b6ff4d7a33c9fd9910c324b25497c0bf53b5
Hyeju Kim <kimoju03@naver.com>
2024-08-14 09:11:27 +0900

    fix: 일정 수정 화면에서 저장되어 있는 장소의 마커가 일부 안 보이는 문제 해결
    - idx 값의 잘못된 사용이 원인
    - 장소 추가 시 마커 번호 이어지게 구현 완료
    
    Related to: #70

--- src/main/resources/static/js/updatetrip.js
+++ src/main/resources/static/js/updatetrip.js
@@ -333,7 +333,7 @@ function addSavedPlacesMarker(latitude, longitude, idx, dayNum) {
         imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
         imgOptions = {
             spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-            spriteOrigin: new kakao.maps.Point(0, ((idx - 1) * 46) + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+            spriteOrigin: new kakao.maps.Point(0, ((idx) * 46) + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
             offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
         },
         markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
@@ -344,7 +344,6 @@ function addSavedPlacesMarker(latitude, longitude, idx, dayNum) {
 
     marker.setMap(map);
     markers[dayNum].push(marker);
-    // setBounds(marker.position);
     console.log('markres: {}', markers);
 
     return marker;
