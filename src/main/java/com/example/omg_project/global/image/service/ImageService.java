70f05fbf951a124fd82a91e53e4e358532ea042a
Hyeju Kim <kimoju03@naver.com>
2024-08-26 14:50:40 +0900

    feat: S3 업로드 서비스 코드 작성
    Related to: #105, #111

--- src/main/java/com/example/omg_project/global/image/service/ImageService.java
+++ src/main/java/com/example/omg_project/global/image/service/ImageService.java
@@ -1,4 +1,12 @@
 package com.example.omg_project.global.image.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface ImageService {
+    String upload(MultipartFile image); // 외부에서 사용할 public 메서드
+    String uploadImage(MultipartFile image);
+    void validateImageFileExtention(String filename);
+    String uploadImageToS3(MultipartFile image) throws IOException;
 }
