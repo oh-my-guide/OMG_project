ebca5c9fcd3780448debc1ff16bcfaef1a10087f
jini <dbwls20202554@gmail.com>
2024-08-28 10:25:51 +0900

    refactor: 필요 없는 패키지 삭제

--- src/main/java/com/example/omg_project/domain/trip/repository/TripLocationRepository.java
+++ src/main/java/com/example/omg_project/domain/trip/repository/TripLocationRepository.java
@@ -1,6 +1,5 @@
 package com.example.omg_project.domain.trip.repository;
 
-import com.example.omg_project.domain.trip.entity.TripDate;
 import com.example.omg_project.domain.trip.entity.TripLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
