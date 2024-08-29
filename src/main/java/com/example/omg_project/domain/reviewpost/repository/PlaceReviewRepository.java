2c36b2353b94df8751d5b69c7301b45c452b6369
seolb <seolb2107@hanmail.net>
2024-08-29 14:18:02 +0900

    feat: 후기게시판 장소별 리뷰 기능 추가
    - 후기 게시글 작성시 장소별 리뷰 기능 추가
    
    Ref: #108

--- src/main/java/com/example/omg_project/domain/reviewpost/repository/PlaceReviewRepository.java
+++ src/main/java/com/example/omg_project/domain/reviewpost/repository/PlaceReviewRepository.java
@@ -3,5 +3,9 @@ package com.example.omg_project.domain.reviewpost.repository;
 import com.example.omg_project.domain.reviewpost.entity.PlaceReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> {
+    List<PlaceReview> findByReviewPostId(Long reviewPostId);
+    void deleteByReviewPostId(Long reviewPostId);
 }
--- src/main/java/com/example/omg_project/domain/reviewpost/repository/ReviewPostRepository.java
+++ src/main/java/com/example/omg_project/domain/reviewpost/repository/ReviewPostRepository.java
@@ -2,11 +2,16 @@ package com.example.omg_project.domain.reviewpost.repository;
 
 import com.example.omg_project.domain.reviewpost.entity.ReviewPost;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewPostRepository extends JpaRepository<ReviewPost, Long> {
+    // 장소별 리뷰도 함께 조회
+    @EntityGraph(attributePaths = {"placeReviews"})
+    Optional<ReviewPost> findWithReviewsById(Long id);
     // 특정 사용자의 전체 게시글 확인
     List<ReviewPost> findReviewPostByUserId(Long userId);
     // 지역별 전체 게시글 조회
