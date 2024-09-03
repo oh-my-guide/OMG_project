b2be01b25e4c91ed072979b57886ef3042fff520
seolb <seolb2107@hanmail.net>
2024-09-03 15:30:10 +0900

    refactor: 퍼블릭IP 설정

--- src/main/java/com/example/omg_project/domain/chat/config/KafkaProducerConfig.java
+++ src/main/java/com/example/omg_project/domain/chat/config/KafkaProducerConfig.java
@@ -23,7 +23,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();   // 설정을 담을 맵 생성
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "54.180.237.173:9092");  // Kafka 서버 주소 설정
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "54.66.163.19:9092");  // Kafka 서버 주소 설정
 
         // 키와 값의 시리얼라이저 설정
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
