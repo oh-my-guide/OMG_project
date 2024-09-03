60098a3c85ddf8278705e0d2d29da8bcea14c876
kyongseo <pokj930@naver.com>
2024-09-03 17:45:20 +0900

    feat: aws

--- src/test/java/com/example/omg_project/OmgProjectApplicationTests.java
+++ src/test/java/com/example/omg_project/OmgProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.omg_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class OmgProjectApplicationTests {
 
     @Test
