f9a930a2430e59547bb3990f25887f5257627ab9
jini <dbwls20202554@gmail.com>
2024-09-04 14:56:43 +0900

    chat IP 수정

--- src/main/java/com/example/omg_project/domain/chat/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/omg_project/domain/chat/config/KafkaConsumerConfig.java
@@ -25,7 +25,7 @@ public class KafkaConsumerConfig {
 
         Map<String, Object> config = new HashMap<>();   // 설정을 담을 맵 생성
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.241.122:9092");  // Kafka 서버 주소 설정
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "15.165.236.189:9092");  // Kafka 서버 주소 설정
 
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "chat-room-listener");   // 그룹 ID 설정
 
